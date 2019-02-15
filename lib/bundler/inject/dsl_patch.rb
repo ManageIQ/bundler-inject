4cdcb257922982277a8e62d272ae7f8b07f4ed4e
Jason Frey <jfrey@redhat.com>
2019-02-15 10:53:39 -0500

    Remove dependencies check as it's not needed
    Previously this was needed because the code could execute in both
    bundler's plugin dsl pass as well as the regular dsl pass.  However,
    this is not possible in plugin form anymore.

--- lib/bundler/inject/dsl_patch.rb
+++ lib/bundler/inject/dsl_patch.rb
@@ -2,23 +2,21 @@ module Bundler
   module Inject
     module DslPatch
       def override_gem(name, *args)
-        if dependencies.any?
-          raise "Trying to override unknown gem #{name}" unless (dependency = dependencies.find { |d| d.name == name })
+        raise "Trying to override unknown gem #{name}" unless (dependency = dependencies.find { |d| d.name == name })
 
-          removed_dependency = dependencies.delete(dependency)
-          if removed_dependency.source.kind_of?(Bundler::Source::Git)
-            @sources.send(:source_list_for, removed_dependency.source).delete_if do |other_source|
-              removed_dependency.source == other_source
-            end
+        removed_dependency = dependencies.delete(dependency)
+        if removed_dependency.source.kind_of?(Bundler::Source::Git)
+          @sources.send(:source_list_for, removed_dependency.source).delete_if do |other_source|
+            removed_dependency.source == other_source
           end
+        end
 
-          calling_file = caller_locations.detect { |loc| !loc.path.include?("lib/bundler") }.path
-          calling_dir  = File.dirname(calling_file)
+        calling_file = caller_locations.detect { |loc| !loc.path.include?("lib/bundler") }.path
+        calling_dir  = File.dirname(calling_file)
 
-          args.last[:path] = File.expand_path(args.last[:path], calling_dir) if args.last.kind_of?(Hash) && args.last[:path]
-          gem(name, *args).tap do
-            warn "** override_gem: #{name}, #{args.inspect}, caller: #{calling_file}" unless ENV["RAILS_ENV"] == "production"
-          end
+        args.last[:path] = File.expand_path(args.last[:path], calling_dir) if args.last.kind_of?(Hash) && args.last[:path]
+        gem(name, *args).tap do
+          warn "** override_gem: #{name}, #{args.inspect}, caller: #{calling_file}" unless ENV["RAILS_ENV"] == "production"
         end
       end
 
