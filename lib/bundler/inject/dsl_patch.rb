7d368f690025f1282c5982d67562165a4269688f
Jason Frey <jfrey@redhat.com>
2019-02-20 21:45:47 -0500

    Refactor into smaller methods

--- lib/bundler/inject/dsl_patch.rb
+++ lib/bundler/inject/dsl_patch.rb
@@ -4,18 +4,11 @@ module Bundler
       def override_gem(name, *args)
         raise "Trying to override unknown gem #{name.inspect}" unless (dependency = find_dependency(name.to_s))
 
-        removed_dependency = dependencies.delete(dependency)
-        if removed_dependency.source.kind_of?(Bundler::Source::Git)
-          @sources.send(:source_list_for, removed_dependency.source).delete_if do |other_source|
-            removed_dependency.source == other_source
-          end
-        end
-
         calling_loc  = caller_locations.detect { |loc| !loc.path.include?("lib/bundler") }
         calling_file = "#{calling_loc.path}:#{calling_loc.lineno}"
-        calling_dir  = File.dirname(calling_file)
 
-        args.last[:path] = File.expand_path(args.last[:path], calling_dir) if args.last.kind_of?(Hash) && args.last[:path]
+        remove_dependencies_and_sources(dependency)
+        expand_gem_path(args, calling_file)
         gem(name, *args).tap do
           warn_override_gem(calling_file, name, args)
         end
@@ -26,7 +19,7 @@ module Bundler
         if !current
           gem(name, *args)
         else
-          version, opts = split_version_opts(args)
+          version, opts = extract_version_opts(args)
           version = [">= 0"] if version.empty?
           if opts.any? || (version != [">= 0"] && Gem::Requirement.new(version) != current.requirement)
             override_gem(name, *args)
@@ -37,9 +30,9 @@ module Bundler
       def to_definition(lockfile, unlock)
         calling_loc = caller_locations(1, 1).first
         if calling_loc.path.include?("bundler/dsl.rb") && calling_loc.base_label == "evaluate"
-          load_bundler_d(File.join(Dir.home, ".bundler.d"))
+          load_global_bundler_d
           @gemfiles.reverse_each do |gemfile|
-            load_bundler_d(File.join(File.dirname(gemfile), "bundler.d"))
+            load_local_bundler_d(File.dirname(gemfile))
           end
         end
         super
@@ -51,14 +44,28 @@ module Bundler
         dependencies.find { |d| d.name == name }
       end
 
-      def split_version_opts(args)
+      def remove_dependencies_and_sources(dependency)
+        removed_dependency = dependencies.delete(dependency)
+        if removed_dependency.source.kind_of?(Bundler::Source::Git)
+          @sources.send(:source_list_for, removed_dependency.source).delete_if do |other_source|
+            removed_dependency.source == other_source
+          end
+        end
+      end
+
+      def expand_gem_path(args, calling_file)
+        return unless args.last.kind_of?(Hash) && args.last[:path]
+        args.last[:path] = File.expand_path(args.last[:path], File.dirname(calling_file))
+      end
+
+      def extract_version_opts(args)
         args.last.is_a?(Hash) ? [args[0..-2], args[-1]] : [args, {}]
       end
 
       def warn_override_gem(calling_file, name, args)
         return if ENV["RAILS_ENV"] == "production"
 
-        version, opts = split_version_opts(args)
+        version, opts = extract_version_opts(args)
         message = "** override_gem(#{name.inspect}"
         message << ", #{version.inspect[1..-2]}" unless version.empty?
         message << ", #{opts.inspect[1..-2]}" unless opts.empty?
@@ -66,6 +73,14 @@ module Bundler
         message = "\e[33m#{message}\e[0m" if $stdout.tty?
 
         warn message
+      end
+
+      def load_global_bundler_d
+        load_bundler_d(File.join(Dir.home, ".bundler.d"))
+      end
+
+      def load_local_bundler_d(dir)
+        load_bundler_d(File.join(dir, "bundler.d"))
       end
 
       def load_bundler_d(dir)
