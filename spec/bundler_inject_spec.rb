ba4907f8be47ad6fca4a8dfdf8ac513f65339cf2
Jason Frey <jfrey@redhat.com>
2019-03-04 16:33:51 -0500

    Rearrange bundle update context and split out Injecting tests

--- spec/bundler_inject_spec.rb
+++ spec/bundler_inject_spec.rb
@@ -12,162 +12,182 @@ RSpec.describe Bundler::Inject do
     G
   end
 
-  shared_examples_for "overrides" do
-    it "with local overrides" do
-      write_bundler_d_file <<~F
-        gem "rack-obama"
-      F
-      bundle(:update, verbose: true)
-
-      expect(out).to match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
-      expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"]]
-    end
-
-    it "with global overrides" do
-      write_global_bundler_d_file <<~F
-        gem "rack-obama"
-      F
-      bundle(:update, verbose: true)
-
-      expect(out).to match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
-      expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"]]
-    end
-
-    it "with local and global overrides" do
-      write_bundler_d_file <<~F
-        gem "rack-obama"
-      F
-      write_global_bundler_d_file <<~F
-        gem "omg"
-      F
-      bundle(:update, verbose: true)
-
-      expect(out).to match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
-      expect(out).to match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
-      expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"], ["omg", "0.0.6"]]
-    end
-
-    it "only outputs \"Injecting\" in verbose mode" do
-      write_bundler_d_file <<~F
-        gem "rack-obama"
-      F
-      write_global_bundler_d_file <<~F
-        gem "omg"
-      F
-      bundle(:update)
-
-      expect(out).to_not match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
-      expect(out).to_not match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
-      expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"], ["omg", "0.0.6"]]
-    end
-
-    describe "#override_gem" do
-      it "with a different version" do
-        write_bundler_d_file <<~F
-          override_gem "rack", "=2.0.5"
-        F
-        bundle(:update)
-
-        expect(lockfile_specs).to eq [["rack", "2.0.5"]]
-        expect(err).to match %r{^\*\* override_gem\("rack", "=2.0.5"\) at .+/bundler\.d/local_overrides.rb:1$}
-      end
-
-      it "with a git repo" do
-        write_bundler_d_file <<~F
-          override_gem "rack", :git => "https://github.com/rack/rack"
-        F
-        bundle(:update)
-
-        expect(lockfile_specs).to eq [["rack", extract_rack_version]]
-        expect(err).to match %r{^\*\* override_gem\("rack", :git=>"https://github.com/rack/rack"\) at .+/bundler\.d/local_overrides.rb:1$}
-      end
-
-      it "with a path" do
-        with_path_based_gem("https://github.com/rack/rack") do |path|
-          write_bundler_d_file <<~F
-            override_gem "rack", :path => #{path.to_s.inspect}
-          F
+  shared_examples_for "bundle update" do
+    context "bundle update" do
+      describe "printing \"Injecting\"..." do
+        it "without verbose" do
+          write_bundler_d_file ""
+          write_global_bundler_d_file ""
           bundle(:update)
 
-          expect(lockfile_specs).to eq [["rack", extract_rack_version(path)]]
-          expect(err).to match %r{^\*\* override_gem\("rack", :path=>#{path.to_s.inspect}\) at .+/bundler\.d/local_overrides.rb:1$}
+          expect(out).to_not match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
+          expect(out).to_not match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
+        end
+
+        it "with verbose" do
+          write_bundler_d_file ""
+          write_global_bundler_d_file ""
+          bundle(:update, verbose: true)
+
+          expect(out).to match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
+          expect(out).to match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
+        end
+
+        it "with local file only" do
+          write_bundler_d_file ""
+          bundle(:update, verbose: true)
+
+          expect(out).to match %r{^Injecting .+/bundler\.d/local_overrides\.rb\.\.\.$}
+        end
+
+        it "with global file only" do
+          write_global_bundler_d_file ""
+          bundle(:update, verbose: true)
+
+          expect(out).to match %r{^Injecting .+/.bundler\.d/global_overrides\.rb\.\.\.$}
         end
       end
 
-      it "with a path that includes ~" do
-        with_path_based_gem("https://github.com/rack/rack") do |path|
-          path = Pathname.new("~/#{path.relative_path_from(Pathname.new("~").expand_path)}")
-
+      describe "#gem" do
+        it "with local file" do
           write_bundler_d_file <<~F
-            override_gem "rack", :path => #{path.to_s.inspect}
+            gem "rack-obama"
           F
           bundle(:update)
 
-          expect(lockfile_specs).to eq [["rack", extract_rack_version(path.expand_path)]]
-          expect(err).to match %r{^\*\* override_gem\("rack", :path=>#{path.expand_path.to_s.inspect}\) at .+/bundler\.d/local_overrides.rb:1$}
+          expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"]]
+        end
+
+        it "with global file" do
+          write_global_bundler_d_file <<~F
+            gem "rack-obama"
+          F
+          bundle(:update)
+
+          expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"]]
+        end
+
+        it "with local and global files" do
+          write_bundler_d_file <<~F
+            gem "rack-obama"
+          F
+          write_global_bundler_d_file <<~F
+            gem "omg"
+          F
+          bundle(:update)
+
+          expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["rack-obama", "0.1.1"], ["omg", "0.0.6"]]
         end
       end
 
-      it "when the gem doesn't exist" do
-        write_bundler_d_file <<~F
-          override_gem "omg"
-        F
-        bundle(:update, expect_error: true)
+      describe "#override_gem" do
+        it "with a different version" do
+          write_bundler_d_file <<~F
+            override_gem "rack", "=2.0.5"
+          F
+          bundle(:update)
 
-        stream = bundler_short_version == "2.0" ? err : out
-        expect(stream).to include "Trying to override unknown gem \"omg\""
+          expect(lockfile_specs).to eq [["rack", "2.0.5"]]
+          expect(err).to match %r{^\*\* override_gem\("rack", "=2.0.5"\) at .+/bundler\.d/local_overrides.rb:1$}
+        end
+
+        it "with a git repo" do
+          write_bundler_d_file <<~F
+            override_gem "rack", :git => "https://github.com/rack/rack"
+          F
+          bundle(:update)
+
+          expect(lockfile_specs).to eq [["rack", extract_rack_version]]
+          expect(err).to match %r{^\*\* override_gem\("rack", :git=>"https://github.com/rack/rack"\) at .+/bundler\.d/local_overrides.rb:1$}
+        end
+
+        it "with a path" do
+          with_path_based_gem("https://github.com/rack/rack") do |path|
+            write_bundler_d_file <<~F
+              override_gem "rack", :path => #{path.to_s.inspect}
+            F
+            bundle(:update)
+
+            expect(lockfile_specs).to eq [["rack", extract_rack_version(path)]]
+            expect(err).to match %r{^\*\* override_gem\("rack", :path=>#{path.to_s.inspect}\) at .+/bundler\.d/local_overrides.rb:1$}
+          end
+        end
+
+        it "with a path that includes ~" do
+          with_path_based_gem("https://github.com/rack/rack") do |path|
+            path = Pathname.new("~/#{path.relative_path_from(Pathname.new("~").expand_path)}")
+
+            write_bundler_d_file <<~F
+              override_gem "rack", :path => #{path.to_s.inspect}
+            F
+            bundle(:update)
+
+            expect(lockfile_specs).to eq [["rack", extract_rack_version(path.expand_path)]]
+            expect(err).to match %r{^\*\* override_gem\("rack", :path=>#{path.expand_path.to_s.inspect}\) at .+/bundler\.d/local_overrides.rb:1$}
+          end
+        end
+
+        it "when the gem doesn't exist" do
+          write_bundler_d_file <<~F
+            override_gem "omg"
+          F
+          bundle(:update, expect_error: true)
+
+          stream = bundler_short_version == "2.0" ? err : out
+          expect(stream).to include "Trying to override unknown gem \"omg\""
+        end
       end
-    end
 
-    describe "#ensure_gem" do
-      it "when the gem doesn't exist" do
-        write_global_bundler_d_file <<~F
-          ensure_gem "omg"
-        F
-        bundle(:update)
+      describe "#ensure_gem" do
+        it "when the gem doesn't exist" do
+          write_global_bundler_d_file <<~F
+            ensure_gem "omg"
+          F
+          bundle(:update)
 
-        expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["omg", "0.0.6"]]
-        expect(err).to_not match %r{^\*\* override_gem}
-      end
+          expect(lockfile_specs).to match_array [["rack", "2.0.6"], ["omg", "0.0.6"]]
+          expect(err).to_not match %r{^\*\* override_gem}
+        end
 
-      it "when overriding without a version" do
-        write_global_bundler_d_file <<~F
-          ensure_gem "rack"
-        F
-        bundle(:update)
+        it "when overriding without a version" do
+          write_global_bundler_d_file <<~F
+            ensure_gem "rack"
+          F
+          bundle(:update)
 
-        expect(lockfile_specs).to eq [["rack", "2.0.6"]]
-        expect(err).to_not match %r{^\*\* override_gem}
-      end
+          expect(lockfile_specs).to eq [["rack", "2.0.6"]]
+          expect(err).to_not match %r{^\*\* override_gem}
+        end
 
-      it "when overriding with the same version" do
-        write_global_bundler_d_file <<~F
-          ensure_gem "rack", "=2.0.6"
-        F
-        bundle(:update)
+        it "when overriding with the same version" do
+          write_global_bundler_d_file <<~F
+            ensure_gem "rack", "=2.0.6"
+          F
+          bundle(:update)
 
-        expect(lockfile_specs).to eq [["rack", "2.0.6"]]
-        expect(err).to_not match %r{^\*\* override_gem}
-      end
+          expect(lockfile_specs).to eq [["rack", "2.0.6"]]
+          expect(err).to_not match %r{^\*\* override_gem}
+        end
 
-      it "when overriding with a different version" do
-        write_global_bundler_d_file <<~F
-          ensure_gem "rack", "=2.0.5"
-        F
-        bundle(:update)
+        it "when overriding with a different version" do
+          write_global_bundler_d_file <<~F
+            ensure_gem "rack", "=2.0.5"
+          F
+          bundle(:update)
 
-        expect(lockfile_specs).to eq [["rack", "2.0.5"]]
-        expect(err).to match %r{^\*\* override_gem\("rack", "=2.0.5"\) at .+/\.bundler\.d/global_overrides.rb:1$}
-      end
+          expect(lockfile_specs).to eq [["rack", "2.0.5"]]
+          expect(err).to match %r{^\*\* override_gem\("rack", "=2.0.5"\) at .+/\.bundler\.d/global_overrides.rb:1$}
+        end
 
-      it "when overriding with other options" do
-        write_global_bundler_d_file <<~F
-          override_gem "rack", :git => "https://github.com/rack/rack"
-        F
-        bundle(:update)
+        it "when overriding with other options" do
+          write_global_bundler_d_file <<~F
+            override_gem "rack", :git => "https://github.com/rack/rack"
+          F
+          bundle(:update)
 
-        expect(lockfile_specs).to eq [["rack", extract_rack_version]]
-        expect(err).to match %r{^\*\* override_gem\("rack", :git=>"https://github.com/rack/rack"\) at .+/\.bundler\.d/global_overrides.rb:1$}
+          expect(lockfile_specs).to eq [["rack", extract_rack_version]]
+          expect(err).to match %r{^\*\* override_gem\("rack", :git=>"https://github.com/rack/rack"\) at .+/\.bundler\.d/global_overrides.rb:1$}
+        end
       end
     end
   end
@@ -245,14 +265,14 @@ RSpec.describe Bundler::Inject do
       expect(out).to include "Installed plugin bundler-inject"
     end
 
-    it "with no overrides" do
+    it "with no files to inject" do
       bundle(:update)
 
       expect(out).to_not match /^Injecting /
       expect(lockfile_specs).to eq [["rack", "2.0.6"]]
     end
 
-    include_examples "overrides"
+    include_examples "bundle update"
     include_examples "bundle check/exec"
   end
 
@@ -269,14 +289,14 @@ RSpec.describe Bundler::Inject do
       expect(out).to_not include "Installed plugin bundler-inject"
     end
 
-    it "with no overrides" do
+    it "with no files to inject" do
       bundle(:update)
 
       expect(out).to_not match /^Injecting /
       expect(lockfile_specs).to eq [["rack", "2.0.6"]]
     end
 
-    include_examples "overrides"
+    include_examples "bundle update"
     include_examples "bundle check/exec"
   end
 end
