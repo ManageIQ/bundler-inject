b643d3cfe9f6a5707028215132fcd975ba84b39d
Keenan Brock <keenan@thebrocks.net>
2023-12-19 17:49:33 -0500

    use __dir__ over __FILE__

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require "colorize"
 
 require "bundler/inject"
 
-Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.include Spec::Helpers
